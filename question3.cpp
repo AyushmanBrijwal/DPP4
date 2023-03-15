0f4397c711b1c2b9ecd7dd8ed314abbbccbe10d6
AyushmanBrijwal <76858741+AyushmanBrijwal@users.noreply.github.com>
2023-03-15 14:38:15 +0530

    Create question3.cpp

---
+++ question3.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+ public:
+    int dp[1000];
+    int func(int len, vector<int> &prices){  //length of the rod ,, pass by reference in a function for price
+    if(len == 0) return 0;
+    if(dp[len] != -1) return dp[len];
+    int ans = 0;
+     for(int len_to_cut = 1; len_to_cut <= prices.size(); ++len_to_cut){
+        if(len - len_to_cut >= 0 ){
+            ans = max(ans,func(len - len_to_cut, prices) + prices[len_to_cut - 1]);
+        }
+     }
+     return dp[len] = ans;
+   }
+
+   int cutRod(int price[], int n) {
+        memset(dp , -1, sizeof(dp));
+        vector<int>prices(price, price+n); // array price will convert into vector
+        return func(n, prices);
+
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+            
+        Solution ob;
+
+        cout << ob.cutRod(a, n) << endl;
+    }
+    return 0;
+}
